cad1e47742eb52d2380eeacd1a3f3c4f7bdc4f93
Saadia El fekak <saadiaelfekak@gmail.com>
2020-12-15 23:10:26 +0000

    creating InputLength function to validate the length of the password

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var characters = "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn0123456789+/*-@%";
-var choiceLowercase = confirm("Do you want to include lowercase letters ?");
-var choiceUppercase = confirm("Do you want to include uppercase letters ?");
-var choiceNumbers = confirm("Do you want to include numbers ?");
-var choiceSpecialCharacters = confirm("Do you want to include special characters (+ / * - @ %) ?");
-var passwordlength = prompt("Please enter a length of at least 8 characters and no more than 128 characters");
+
 
 // Write password to the #password input
 function writePassword() {
@@ -19,8 +15,20 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
 
-choiceLowercase 
-choiceUppercase 
-choiceNumbers 
-choiceSpecialCharacters
-passwordlength
+function inputLength() {
+var passwordlength = prompt("Please enter the length of the password");
+do {
+if (passwordlength < 8 || passwordlength > 128 ) {
+  alert("Please enter a length of at least 8 characters and no more than 128 characters");
+  passwordlength = prompt("Please reenter the length of the password");
+};
+} while (passwordlength < 8 || passwordlength > 128 )
+};
+
+inputLength()
+
+var choiceLowercase = confirm("Do you want to include lowercase letters ?");
+var choiceUppercase = confirm("Do you want to include uppercase letters ?");
+var choiceNumbers = confirm("Do you want to include numbers ?");
+var choiceSpecialCharacters = confirm("Do you want to include special characters (+ / * - @ %) ?");
+

a2317cb1610b4718653e19b73372fdd7d8571964
Saadia El fekak <saadiaelfekak@gmail.com>
2020-12-18 13:45:36 +0000

    Updating GeneratePassword function

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 var passwordlength = prompt("Please enter the length of the password");
+var choiceLowercase = confirm("Do you want to include lowercase letters ?");
+var choiceUppercase = confirm("Do you want to include uppercase letters ?");
+var choiceNumbers = confirm("Do you want to include numbers ?");
+var choiceSpecialCharacters = confirm("Do you want to include special characters ?");
 
 
 // Write password to the #password input
@@ -17,40 +21,42 @@ generateBtn.addEventListener("click", writePassword);
 
 // Input password length and assign this length to the password textarea
 function inputLength() {
-
 do { 
-
  if (passwordlength < 8 || passwordlength > 128 ) {
-
   alert("Please enter a length of at least 8 characters and no more than 128 characters");
   passwordlength = prompt("Please reenter the length of the password")};
-
 } while (passwordlength < 8 || passwordlength > 128 );
 
 document.getElementById("password").maxLength = passwordlength;
- 
-};
+ };
 
 //Generate random password
 function generatePassword(passwordlength) {
+  
   var password = "" ;
-  var characters = "AZERTYUIOPQSDFGHJKLMWXCVBNazertyuiopqsdfghjklmwxcvbn0123456789+/*-@%";
- 
-  for (var i = 0 ; i < passwordlength ; i++)   {
+  var characters = "";
+  var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var specialCharacters = "-_&@%*$+=#";
 
-  password += characters.charAt(Math.floor(Math.random() * characters.length));
- 
-  }
-  return password;
+  if(choiceLowercase == true) {
+  characters+=lowerCase };
+  
+  if(choiceUppercase == true) { 
+    characters+=upperCase };
+  
+  if(choiceNumbers == true) {
+  characters+=numbers };
+  
+  if(choiceSpecialCharacters == true) {
+  characters+=specialCharacters };
 
+   for (var i = 0 ; i < passwordlength ; i++)   {
+    password += characters.charAt(Math.floor(Math.random() * characters.length));
+   }
+   return password;
 };
 
 
 inputLength()
-generatePassword(passwordlength)
-
-var choiceLowercase = confirm("Do you want to include lowercase letters ?");
-var choiceUppercase = confirm("Do you want to include uppercase letters ?");
-var choiceNumbers = confirm("Do you want to include numbers ?");
-var choiceSpecialCharacters = confirm("Do you want to include special characters (+ / * - @ %) ?");
-
